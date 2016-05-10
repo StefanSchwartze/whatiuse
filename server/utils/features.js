dd7f50ea1bdd0d6a6cba037eea4dae98423ee9fa
Stefan <stefan.schwartze@gmail.com>
2016-05-10 17:51:21 +0200

    outsourced feature detection

---
+++ server/utils/features.js
@@ -0,0 +1,83 @@
+import JSONStream from 'JSONStream';
+import { pipe, through, concat } from 'mississippi';
+import styles from 'style-stream';
+import next from 'next-stream';
+import doiuse from 'doiuse/stream';
+import prune from './prune';
+import unique from './unique';
+import limitstream from './limit';
+
+/*
+* options must have `url`.
+*/
+module.exports = function evaluate(args) {
+
+	return new Promise((resolve, reject) => {
+
+		let url = args.url || '';
+		let css = args.css || '';
+		let browsers = args.browser || '';
+		let streams = [styles({ url: url })];
+		let errorsAndWarnings = [];
+		let uniq = unique();
+		let limit = limitstream(1e6);
+		let features = prune();
+
+		streams = streams.concat([
+			limit,
+			doiuse({ browsers: browsers, skipErrors: true }, url.trim().length ? url : 'pasted content')
+			.on('warning', function (warn) { 
+				errorsAndWarnings.push(warn) 
+			}),
+			uniq.features,
+			features
+		]);
+		let stringify = features.pipe(JSONStream.stringify());
+		let error = through();
+
+		pipe(streams, 
+			function (err) {
+				if (err) {
+					console.error('Error processing CSS', err);
+					console.trace();
+					if (!limit.ended) { 
+						limit.end() 
+					}
+					if (!uniq.ended) { 
+						uniq.features.end() 
+					}
+					stringify.end();
+					if (JSON.stringify(err) === '{}') { 
+						err = err.toString() 
+					}
+					errorsAndWarnings.push(err)
+				}
+				error.end(', "errors":' + JSON.stringify(errorsAndWarnings));
+			}
+		)
+				
+		let usageData = [];
+		const concatStream = next([
+			'{ "args":', JSON.stringify(args), ',',
+			'"usages": ', stringify.pipe(through()), ',',
+			'"counts": ', uniq.counts, ',',
+			'"size": ', limit.size,
+			error,
+			'}'
+			], { open: false }
+		);
+		const finalStream = JSONStream.parse();
+		concatStream.pipe(finalStream);
+
+		finalStream.on('data', (data) => {
+			usageData.push(data);
+		});
+		finalStream.on('error', (err) => {
+			reject(err);
+		});
+		finalStream.on('end', (err) => {
+			resolve(usageData);
+		});
+
+	});
+}
