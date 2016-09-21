e848422715e30819bbdda81f801b87ca0c13d648
Stefan <stefan.schwartze@gmail.com>
2016-09-21 17:00:18 +0200

    fixed tooltip for detailed timeline

--- app/components/dashboard/statistics/detailtimeline.jsx
+++ app/components/dashboard/statistics/detailtimeline.jsx
@@ -20,16 +20,36 @@ export default class DetailTimeline extends React.PureComponent {
 		const toPercent = (decimal, fixed = 0) => {
 			return `${(decimal * 100).toFixed(fixed)}%`;
 		};
+		const renderTooltipContent = (o) => {
+			const { payload, label } = o;
+			console.log(label);
+			return (
+				<div className="history-tooltip">
+					<p className="label">{`${label}`}</p>
+					<ul className="list">
+						{
+							payload.map((entry, index) => (
+								<li className="" key={`item-${index}`} style={{color: entry.color}}>
+								{`${entry.name}: ${entry.value.toFixed(2)}%`}
+								</li>
+							))
+						}
+					</ul>
+				</div>
+			);
+		};
+		
 		const snapshots = this.props.snapshots.map((item, index) => {
 			return {
 				missingSupport: item.missingSupport,
 				partialSupport: item.partialSupport,
-				fullSupport: 100 - item.missingSupport - item.partialSupport
+				fullSupport: 100 - item.missingSupport - item.partialSupport,
+				captured: moment(item.captured).calendar()
 			}
 		});
 		const lastCheck = this.props.isChecking ? 
 			(<span><i className="icon icon-spinner8 animate rotate"></i>Currently checking…</span>) : 
-			(<span>{moment(snapshots[snapshots.length - 1].captured).calendar()}</span>);
+			(<span>{snapshots[snapshots.length - 1].captured}</span>);
 
 		return (
 			<div className="history-container">
@@ -41,9 +61,11 @@ export default class DetailTimeline extends React.PureComponent {
 				<div className="chart chart--extended">
 					<ResponsiveContainer>
 
-						<AreaChart width={1000} height={600} data={snapshots} stackOffset="expand">
-							<XAxis dataKey="captured"/>
-							<YAxis tickFormatter={toPercent}/>
+						<AreaChart width={1000} height={150} data={snapshots}
+							margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+							<XAxis dataKey="captured" />
+							<YAxis />
+							<Tooltip content={renderTooltipContent}/>
 							<CartesianGrid strokeDasharray="3 3"/>
 							<Area type='monotone' dataKey='fullSupport' stackId="1" stroke='#82ca9d' fill='#82ca9d' />
 							<Area type='monotone' dataKey='partialSupport' stackId="1" stroke='#ffc658' fill='#ffc658' />
