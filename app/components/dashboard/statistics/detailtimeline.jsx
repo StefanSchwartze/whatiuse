0b8a38fc7d68f9c7d88c8b84fbe176c8ceb35a97
Stefan <stefan.schwartze@gmail.com>
2016-11-02 12:10:52 +0100

    updated recharts, deactivated tooltip because of crashes in chrome

--- app/components/dashboard/statistics/detailtimeline.jsx
+++ app/components/dashboard/statistics/detailtimeline.jsx
@@ -60,7 +60,7 @@ export default class DetailTimeline extends React.PureComponent {
 							margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
 							<XAxis dataKey="captured" />
 							<YAxis />
-							<Tooltip content={renderTooltipContent}/>
+							{/*<Tooltip content={renderTooltipContent}/>*/}
 							<CartesianGrid strokeDasharray="3 3"/>
 							<Area type='monotone' dataKey='fullSupport' stackId="1" stroke='#82ca9d' fill='#82ca9d' />
 							<Area type='monotone' dataKey='partialSupport' stackId="1" stroke='#ffc658' fill='#ffc658' />
